9b7bcca58f034fbf8302c4afb93113346cd9d335
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-23 17:59:52 +0600

    design and make the list

--- votingapp/views.py
+++ votingapp/views.py
@@ -1,4 +1,28 @@
+from http.client import HTTPResponse
+import imp
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
+lang = ['Java', 'C', 'Python', 'C++', '.NET', 'C#', 'PHP', 'JavaScript', 'SQL', 'Objective-C', 'Delphi', 'Ruby', 'MATLAB', 'Assembly language', 'Swift', 'Go', 'Perl', 'R', 'SQL', 'Visual Basic', 'SAS', 'Dart', 'F#', 'COBOL', 'Scala', 'ABAP', 'Fortran', 'Lua', 'Rust', 'Lisp', 'Groovy', 'LabVIEW', 'Prolog', 'Ada', 'Julia', 'Haskell', 'Apex', 'Kotlin', 'Bash', 'Ladder Logic', 'Clojure', 'Scheme']
+
+globalcnt = dict()
 
 def index(request):
-   return render(request, 'index.html')
+   
+   dict = {
+      "lang" : lang
+   }
+   return render(request, 'index.html', context=dict)
+
+def getquery(request):
+   q = request.GET['languages']
+   if q in globalcnt:
+      globalcnt[q] = globalcnt[q]+1
+   else:
+      globalcnt[q] = 1
+   
+   dict = {
+      "lang" : lang,
+      "globalcnt" : globalcnt
+   }
+   return render(request, 'index.html', context=dict)
